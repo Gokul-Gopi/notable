3dbc9fab104aa49cc688ed37eb5322e054934cec
Gokul Gopi <gokulg1412@gmail.com>
2022-09-25 14:05:29 +0530

    feat(server): connection to db established

--- server/index.js
+++ server/index.js
@@ -1,14 +1,18 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectToMongoDB } from "./utils/connectToDB.js";
 const app = express();
 dotenv.config();
+connectToMongoDB();
 
 app.get("/", (req, res) => {
-  return res.send("Server running on port 5000");
+  return res.send("Server is running..");
 });
 
 const PORT = process.env.PORT || 5000;
 const ENVIRONMENT = process.env.NODE_ENV;
 app.listen(PORT, () => {
-  return console.log(`server running on port ${PORT} in ${ENVIRONMENT} mode..`);
+  return console.log(
+    `⚡️ Server is running on port ${PORT} in ${ENVIRONMENT} mode..`
+  );
 });
